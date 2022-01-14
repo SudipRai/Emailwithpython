0711f9c29f4147ab8966a0633ee1571d8281e86f
Sudip Rai <raisudip05@gmail.com>
2022-01-14 11:26:35 +0545

    image as attachement

--- newemail.py
+++ newemail.py
@@ -4,6 +4,7 @@ from os.path import basename
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
+from email.mime.image import MIMEImage
 
 
 from_addr='raisudip0005@gmail.com'
@@ -22,7 +23,12 @@ filename='test.txt'
 with open(filename,'r') as f:
     attachment=MIMEApplication(f.read(), Name=basename(filename))
     attachment['Content-Disposition']='attachement; filename="{}"'.format(basename(filename))
+
+with open('nepse.jpg','rb') as f:
+    file_data=f.read()
+    attachment1=MIMEImage(file_data, Name=basename('nepse.jpg'))
 msg.attach(attachment)
+msg.attach(attachment1)
 server=smtplib.SMTP_SSL('smtp.gmail.com')
 server.login(from_addr,'helpmejesus1')
 server.send_message(msg,from_addr=from_addr,to_addrs=[to_addr])
